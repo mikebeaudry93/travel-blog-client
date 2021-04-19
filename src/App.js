30582d205e403d782c3add1150f51a9c9a6ceb54
Mike Beaudry <michaelbeaudry93@gmail.com>
2021-04-19 16:22:26 -0400

    scss adjusted on several components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./app.scss";
 
-
 // components
 import Router from "./Router";
 
 // context
 import { UserContextProvider } from "./context/UserContext";
-
 
 function App() {
   return (
